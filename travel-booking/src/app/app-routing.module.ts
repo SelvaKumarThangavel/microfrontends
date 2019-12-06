94cb1f369a241edc730410d36504975279acea03
SelvaKumarThangavel <54984488+SelvaKumarThangavel@users.noreply.github.com>
2019-12-06 11:41:09 +0530

    microfrontends base code

---
+++ travel-booking/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_guards/auth-guard';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path : '', component : LoginComponent},
+  {path : 'login', component : LoginComponent, canActivate: [AuthGuard]},
+  {path : 'home', component : HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents=[LoginComponent];
